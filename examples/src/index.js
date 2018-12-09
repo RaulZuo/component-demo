83901dd73e27334c0faf52e2aed7d5171292d5e8
佐侑 <zuobin.zb@alibaba-inc.com>
2018-12-10 02:14:33 +0800

    feat: init project

---
+++ examples/src/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import React from 'react';
+import { render } from 'react-dom';
+import ComponentDemo from '../../src';
+const App = () => (
+  <ComponentDemo />
+);
+render(<App />, document.getElementById("root"));
---
+++ src/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import React, { Component } from 'react';
+import './styles.css';
+
+class ComponentDemo extends Component {
+  render() {
+    return <h1>Hello from Component Demo</h1>
+  }
+}
+
+export default ComponentDemo;
