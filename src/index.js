4c3ec75a65c001ecd38ee197d44715b146d23f73
RaulZuo <zuobin_nju@163.com>
2018-12-10 10:48:24 +0800

    feat: add git

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './styles.css';
 
 class ComponentDemo extends Component {
   render() {
-    return <h1>Hello from Component Demo</h1>
+    return <h1>Hello from Component Demo  </h1>
   }
 }
 
